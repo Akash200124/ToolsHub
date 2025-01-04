d8330d0c3b7d2bd3b83594e79841347fd22114f0
unknown <akashkumar200124@gmail.com>
2025-01-05 02:47:37 +0530

    changes in the json formatter

--- src/Tools/JsonParser.jsx
+++ src/Tools/JsonParser.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 
 import JSONPretty from 'react-json-pretty';
+import { JSONTree } from 'react-json-tree';
 
 import '../App.css'
 
@@ -8,16 +9,18 @@ import 'react-json-pretty/themes/monikai.css';
 function JsonParser() {
     const [text, setText] = useState('')
     const [output, setOutput] = useState('')
+
+    const [format, setFormat] = useState(false);
     const [isChecked, setIsChecked] = useState(false);
 
 
-  
     const handleToggle = () => {
         setIsChecked(!isChecked);
     };
 
 
     const parseJson = () => {
+        setFormat(false);
         try {
             const parsed = JSON.parse(text);
             setOutput(JSON.stringify(parsed, null, 2));
@@ -30,27 +33,30 @@ function JsonParser() {
 
 
     const formatJson = () => {
+        setFormat(true);
         try {
-            const formatted = text;
-            setOutput(formatted);
-            console.log("formatted:", formatted)
+            const parsed = JSON.parse(text);
+            setOutput(parsed);
         } catch (error) {
-            console.error('Error formatting JSON:', error);
-            setOutput('Error formatting JSON');
+            console.error('Error parsing JSON:', error);
+            setOutput('Invalid JSON' + "  " + error);
         }
     };
 
+
+
+
     return (
-        <div className='d-flex flex-row justify-content-center align-items-center my-5 '>
-            <div className='me-5'>
+        <div className='d-flex  flex-row justify-content-center align-items-center my-5 '>
+            <div className='me-5 '>
                 <textarea
-                    rows="10"
-                    cols="60"
+                    rows="20"
+                    cols="80"
                     className=" my-3 w-100 h-100"
                     wrap="off"
                     autoCorrect="off"
                     autoCapitalize="off"
-                    spellCheck={false}
+                    spellCheck={true}
                     aria-autocomplete="both"
                     role="textbox"
                     value={text}
@@ -58,39 +64,39 @@ function JsonParser() {
                 ></textarea>
             </div>
             <div className='my-3 d-flex flex-column '>
-                <label className="switch my-3">
-                    <input type="checkbox" checked={isChecked} onChange={handleToggle} />
-                    <span className="slider"></span>
-                </label>
+                
 
                 <button
                     className='btn btn-primary'
                     onClick={parseJson}
                 >json parser</button>
+
                 <button
                     className='btn btn-secondary my-2'
                     onClick={formatJson}
                 >formatter json</button>
             </div>
-            <div>
-                { }
-                <textarea
-                    rows="10"
-                    cols="60"
-                    className=" ms-5  w-100 h-100"
-                    wrap="off"
-                    autoCorrect='off'
-                    autoCapitalize="off"
-                    spellCheck="false"
-                    aria-autocomplete="both"
-                    role="textbox"
-                    value={output}
-                >
+            
 
-                </textarea>
-                <JSONPretty id="json-pretty" data={output}></JSONPretty>
 
+            <div className='json-output ms-5'>
+                {!format ? (
+                    <textarea
+                        className="w-100 h-100"
+                        wrap="off"
+                        autoCorrect="off"
+                        autoCapitalize="off"
+                        spellCheck="false"
+                        aria-autocomplete="both"
+                        role="textbox"
+                        value={output}
+                        readOnly
+                    ></textarea>
+                ) : (
+                    <JSONTree data={output} invertTheme={false} />
+                )}
             </div>
+
         </div>
     )
 }
